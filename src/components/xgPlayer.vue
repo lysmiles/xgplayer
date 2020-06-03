b1846895a36b7a8df5c33ffaee5c2d8aa2e2bc0e
liyan <ly199807>
2020-06-03 15:52:46 +0800

    first commit

---
+++ src/components/xgPlayer.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="hello">
+    <div id="videoId">
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+  import Player from 'xgplayer';
+
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        player: '',
+
+      }
+    },
+    created() {
+
+    },
+    mounted() {
+      this.player = new Player({
+        id: 'videoId',
+        url: 'http://s2.pstatp.com/cdn/expire-1-M/byted-player-videos/1.0.0/xgplayer-demo.mp4',
+        // url: 'rtsp://Inphase:2KE496r8a@182.145.195.233:9500/DevCode=08201021122A584E400/DevAor=08201021122A584E400/StreamIndex=0',
+        height: 340,
+        width: 600,
+        screenShot: true,
+        playsinline: true,
+        whitelist: [],
+        danmu: {
+          comments: [
+            {
+              duration: 15000,
+              id: "2",
+              start: 3000,
+              txt: "长弹幕长弹幕长弹幕",
+              mode: "top"
+            },
+          ],
+          area: {
+            start: 0,
+            end: 1
+          },
+          closeDefaultBtn: false,
+          defaultOff: false,
+          panel: false
+        },
+
+      });
+    },
+    methods: {
+      initXGPlayer() {
+
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ src/pages/home/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="play-video">
+    <div class="left-tree">
+      摄像头切换
+    </div>
+    <div class="right-play">
+
+      <div class="play-content">
+        <xg-player></xg-player>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import xgPlayer from "../../components/xgPlayer";
+  export default {
+    name: "index",
+    components:{
+      xgPlayer,
+    },
+    data() {
+      return {
+        videoUrl:'http://s2.pstatp.com/cdn/expire-1-M/byted-player-videos/1.0.0/xgplayer-demo.mp4'
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .play-video {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+
+    .left-tree {
+      width: 25%;
+      height: 100%;
+      background-color: #f1f1f1;
+    }
+
+    .right-play {
+      width: 75%;
+      height: 100%;
+
+      .play-content {
+        width: 100%;
+        height: 580px;
+        background-color: #cecece;
+      }
+    }
+  }
+</style>
